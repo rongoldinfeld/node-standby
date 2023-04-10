9c63f0842fdef336b6bcf32cf6c591d584708ef4
Ron Goldinfeld <rongoldinfeld56@gmail.com>
2023-04-10 15:43:42 +0300

    re-run ci

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,6 @@ import { describe, it, expect } from "vitest"
 
 describe('Pass CI', () => { 
     it('should go true', () => {
-        expect(1).toEqual(1);
+        expect(2).toEqual(2);
     })
  })
