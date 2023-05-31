527bb906ae1a657bdf6d2aa9d6643d1b75e24551
Ron Goldinfeld <rongoldinfeld56@gmail.com>
2023-05-31 17:12:15 +0300

    changed file name type and refactor using prettier (#8)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import {Client} from "node-zookeeper-client";
-import {registerCallback} from "./zookeeper/leader-electron";
+import { Client } from "node-zookeeper-client";
+import { registerCallback } from "./zookeeper/leader-election";
 
-export const standby = (
-    client: Client,
-    fn: Function,
-) => registerCallback(client, fn);
+export const standby = (client: Client, fn: Function) =>
+  registerCallback(client, fn);
